bd3a74af9f6308b75b48f945ca4d875c29e993a9
Linwl <304115325@qq.com>
2018-10-26 17:34:55 +0800

    增加插值查找算法

--- searchTools.py
+++ searchTools.py
@@ -33,6 +33,33 @@ def binary_search(arry,item):
             low = middle + 1
     return '待查找元素%s不存在指定列表中'%item
 
+@cal_time
+def interpolation__search(arry,item):
+    """
+    插值查找算法
+    :param arry: 源数组
+    :param item: 目标
+    :return:
+    """
+    low = 0
+    high = len(arry) -1
+    arry = SortTools.quick_sort(arry)
+    time = 0
+    while low < high:
+        time += 1
+        # 计算mid值是插值算法的核心代码
+        mid = low + int((high - low) * (item - arry[low]) / (arry[high] - arry[low]))
+        if arry[mid] == item:
+            # 打印查找的次数
+            print("times: %s" % time)
+            return '待查元素{0}在列表中下标为：{1}'.format(item, mid)
+        elif item < arry[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return '待查找元素%s不存在指定列表中'%item
+
 if __name__ == '__main__':
     arry = [4,5,9,3,1,7]
     print(binary_search(arry,4))
+    print(interpolation__search(arry, 4))
