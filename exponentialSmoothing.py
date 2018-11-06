3508e8131381477556a9de34a8f21d264171e02b
Linwl <304115325@qq.com>
2018-11-06 09:28:29 +0800

    修改平滑预测

--- exponentialSmoothing.py
+++ exponentialSmoothing.py
@@ -20,7 +20,9 @@ def read_excl():
     append = datas.append
     for i in range(0, len(df)):
         meter = dict()
-        meter.setdefault('MeterAddr', str(df.iloc[i].get('表计表号')))
+        meteraddr= str(df.iloc[i].get('表计表号'))
+        meteraddr = meteraddr.rjust(12,'0')
+        meter.setdefault('MeterAddr', meteraddr)
         meter.setdefault('DataTime',str(df.iloc[i].get('数据时间')))
         meter.setdefault('Value', float(df.iloc[i].get('电表余额')))
         append(meter)
@@ -78,7 +80,7 @@ if __name__ == '__main__':
     i =0
     weight = 0.6
     for d in source:
-        if d.get('MeterAddr') == '101735008529':
+        if d.get('MeterAddr') == '521233302766':
             d['DataTime'] = datetime.strptime(d.get('DataTime'),'%Y-%m-%d %H:%M:%S')
             append(d)
             i+=1
@@ -94,13 +96,17 @@ if __name__ == '__main__':
         print('{0}的一次平滑为{1}'.format(data.get('DataTime'), predicteds.get(data.get('DataTime').strftime('%Y-%m-%d'))))
         print('{0}的二次平滑为{1}'.format(data.get('DataTime'), second_predicteds.get(data.get('DataTime').strftime('%Y-%m-%d'))))
     print()
-    pre_day = datetime(2018,10,27)
+    pre_day = datetime(2018,11,2)
     str_pre_day = pre_day.strftime('%Y-%m-%d')
     a = 2 * predicteds.get(str_pre_day) - second_predicteds.get(str_pre_day)
     c = predicteds.get(str_pre_day) - second_predicteds.get(str_pre_day)
     b = (weight/(1-weight))*c
     t = a + b * 1
+    e = datas[-3].get('Value')
+    print('实际值为',e)
     print('{0}的预测值:{1}'.format(str_pre_day,t))
+    d = (t-e)/(e/100)
+    print('误差值为',d)
     t = a + b * 2
     str_pre_day = (pre_day + timedelta(days=1)).strftime('%Y-%m-%d')
     print('{0}的预测值:{1}'.format(str_pre_day, t))
